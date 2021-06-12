ba4835e2a7a2b4b5eb01b1e51aed5acfb9e5c62a
ProjectSorcerer <ProjectSorcerer@users.noreply.github.com>
2021-06-12 14:30:28 -0600

    Massive addjustments
    Just prepings and cleaning up.
    And I added trello board link to the readme to see what I want to do and what ill be working on in the future.

--- main.py
+++ main.py
@@ -16,27 +16,3 @@ def payday_function():
 
 payday_function()
 
-def newbills_fuctions():
-
-#Print User Input to A File
-#Inputs of Name,Cost,Due day to be write down and pulled back up from file and to be read in another fuction ytbn
-#In console to save the cbill amount in total with other bills that I got going.
-#My pay day is the 5th and 20th of every month, so if my bills fall after one of those days
-#Take all cbills that are due by checking dbill, but not after the other day. Just in their paid periods
-    nbill = input("Name: ")
-    cbill = input("Cost: ")
-    dbill = input("Day of Month: ")
-y = open('C:\Users\USERNAME\Documents\Bills.txt', 'w')
-#I want to write more then one thing but being able to recall them back as independent variables
-y.write(nbill)
-
-#Convert text to binary and then write to file
-#Guess this has to exist i dont know i was skimming and reading bits and piecees
-nbill = input("Name: ")
-cbill = input("Cost: ")
-dbill = input("Day of Month: ")
-y = open('C:\Users\USERNAME\Documents\Bills.txt', 'wb')
-#I want to write more then one thing but being able to recall them back as independent variables
-y.write(nbill)
-
-newbills_fuctions()
