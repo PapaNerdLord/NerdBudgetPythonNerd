8bc3eee4b75b92e72ea57caa64bfcdffb226e326
ProjectSorcerer <ProjectSorcerer@users.noreply.github.com>
2021-06-12 13:59:54 -0600

    Create test.py
    Added test file for things I want to try and like a blue print kind of like a playground or like Minecraft creative world for heavily modded stuff...

---
+++ test.py
@@ -0,0 +1,9 @@
+dpaid = 4
+def savings_percentage_options_function()
+
+# dpaid = days paid, like you get paid twice a month or three times a month.
+	
+	print("Current amount of paydays with in a month is " + dpaid + " times a month.")
+
+
+savings_percentage_options_function()
